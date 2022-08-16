734f4d0c7bb03efb9e8919b1267d6092da489fb6
Juan Camilo <jccifuentes21@gmail.com>
2022-08-15 22:20:03 -0700

    finished project of realistic example of custom hooks

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,35 @@
+import useHttp from "../../hooks/use-http";
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name;
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = (taskText) => {
+    sendTaskRequest(
+      {
+        url: "https://react-http-1336a-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        header: { "Content-Type": "application/JSON" },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
